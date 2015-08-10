80f9478c20c00cc68088d2543adf361b5e1c354c
Owen Jow <owenjow@berkeley.edu>
2015-08-09 20:20:50 -0700

    Working on CORS support

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # hacker-headlines
 Hacker Headlines is a Chrome extension that periodically checks [Hacker News](https://news.ycombinator.com/) for the latest and greatest updates on the world of computer science. If it finds something that readers have deemed acceptably newsworthy, a notification containing the headline and link will be displayed in the corner of your browser window. 
 
-This way, you don't need to spend every other minute checking the website for distractions.<sup>1</sup> And in the meantime, you can learn new things without even trying. Yay!
+That means you don't need to spend every other minute checking the site for distractions.<sup>1</sup> And in the meantime, you can learn new things without even trying. Isn't that great?
+
+### For the visually inclined
+Here's a screenshot of Hacker Headlines in action:
+[IMAGE COMING SOON]
 
 ***
 <sup>1</sup> Instead, the distractions will come to you (...but in a controlled setting!)
--- background.js
+++ background.js
@@ -10,21 +10,42 @@ const NEWS_URL = 'https://news.ycombinator.com/';
 const PT_BENCHMARK = 70;
 const NOTIF_SOUND_PATH = 'sounds/waterSplashing.mp3';
 const STD_ALARM_NAME = 'c.alarm'; // "check" alarm
-const STD_PERIOD = 20; // this is how often the check should happen
+const STD_PERIOD = 20; // this (in minutes) is how often the check should happen
 const STD_NOTIF_NAME = 'u.notification'; // "update" notification
 const NOTIF_DELAY = 15000; // ms until we close the notification automatically
 const NOTIF_ICON_PATH = 'images/hacker_news.png';
-var id = 0; // the id for each notification
 
-// Checks Hacker News for topics that have over PT_BENCHMARK points
-function checkForUpdates() {
-    /* INSERT UPDATE-CHECKING LOGIC HERE */
-    /* presumably AFTER my final on Thursday */
+var id = 0; // the id for each notification
+var oldNews = Object.create(null); // set of headlines that have already been displayed
+var day = Date.prototype.getDay();
+
+// Gets the biggest news that hasn't been seen before.
+function getBiggestNews() {
+    var rText = this.responseText;
+    console.log(rText);
+}
+
+/* Checks Hacker News for new topics that have over PT_BENCHMARK points.
+ * ON_CLICK is a boolean value that specifies whether or not this check
+ * originated from a click. */
+function checkForUpdates(onClick) {
+    // Send an HTTP request to Hacker News
+    var xhr = new XMLHttpRequest();
+    xhr.addEventListener('load', getBiggestNews);
+    xhr.open('GET', NEWS_URL, true);
+    xhr.setRequestHeader('Access-Control-Allow-Origin', "*");
+    xhr.send();
     
-    // If it finds a hot topic, throw out a notification!
-    if (true) { // (testing notifications atm)
-        createNotification();
+    // If a hot topic was found, throw out a notification!
+    if (/* its score is >= PT_BENCHMARK */) {
+        // oldNews[/* LABEL */] = true; // this news is now old news
+        createNotification('[Hacker News title]', '[link]');
         makeSound();
+    } else if (onClick) {
+        /* If we can't find anything new that's over PT_BENCHMARK points
+         * AND the user clicked on the icon, then we'll settle for the 
+         * highest-rated news on the front page that isn't old news. */
+        getBiggestNews();
     }
 }
 
@@ -41,11 +62,12 @@ function startAlarm() {
     });
 }
 
-function createNotification() {
+// Creates a notification with TITLE as the title and LINK as the message.
+function createNotification(title, link) {
     chrome.notifications.create(STD_NOTIF_NAME + id, {
         type: 'basic',
-        title: '[HACKER NEWS TITLE]',
-        message: '[link]',
+        title: title, // the name of the article
+        message: link, // a link to the article
         iconUrl: NOTIF_ICON_PATH
     }, function() {});
     
@@ -57,17 +79,28 @@ function createNotification() {
     id++; // now that we're done with this iteration of the notification id, update it
 }
 
-// When the icon is clicked, check for updates and reset the alarm timer
-chrome.browserAction.onClicked.addListener(function() {
-    checkForUpdates();
-    
-    // Reset el alarm (because we just checked!)
-    chrome.alarms.clear(STD_ALARM_NAME);
+// We'll check for updates and start the first alarm
+chrome.runtime.onStartup.addListener(function() {
+    checkForUpdates(false);
     startAlarm();
 });
 
-// We'll check for updates and start the first alarm
-chrome.runtime.onLaunched.addListener(function() {
-    checkForUpdates();
+// Every time an alarm rings, look for hot topics that we haven't seen before
+chrome.alarms.onAlarm.addListener(function() {
+    // The oldNews set should be cleared by the day
+    if (Date.prototype.getDay() != day) { // meaning: is it a new day?
+        oldNews = Object.create(null);
+        day = Date.prototype.getDay();
+    }
+    
+    checkForUpdates(false);
+})
+
+// When the icon is clicked, check for updates and reset the alarm timer
+chrome.browserAction.onClicked.addListener(function() {
+    checkForUpdates(true);
+    
+    // Reset el alarm (because we just checked!)
+    chrome.alarms.clear(STD_ALARM_NAME);
     startAlarm();
 });
